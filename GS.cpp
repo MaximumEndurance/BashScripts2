6ea902ce4d97a6e70d6002800fd3f8e06ff52827
Siddharth Dangwal <maximumendurance1965@gmail.com>
2017-03-10 23:50:53 +0530

    gs
    hi

---
+++ GS.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class transaction       // contains information about all the transactions happening.
+{
+    long long timestamp;
+    long long acct_ID;
+    string trans_ID;
+    string trader_ID;
+public:
+    void rollback( int tID )
+    {
+
+    }
+};
+
+class accounts
+{
+    string trans_ID[];
+
+};
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    while(n--)
+    {
+
+    }
+}
